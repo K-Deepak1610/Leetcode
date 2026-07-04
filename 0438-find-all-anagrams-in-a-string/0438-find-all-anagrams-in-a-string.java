c4be2ece8a66bed05878139d658144fc8af75a4d
K Deepak <deepakkarthik20@gmail.com>
2026-07-04 18:04:54 +0530

    Time: 31 ms (31.34%), Space: 47.2 MB (28.66%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -4,28 +4,28 @@ class Solution {
         if(s.length() < p.length()){
             return result;
         }
-        Map<Character,Integer> sMap = new HashMap<>();
-        Map<Character,Integer> pMap = new HashMap<>();
+        Map<Character,Integer> map = new HashMap<>();
 
         for(char c :p.toCharArray()){
-            pMap.put(c,pMap.getOrDefault(c,0)+1);
+            map.put(c,map.getOrDefault(c,0)+1);
         }
         int left = 0;
         int count = p.length();
         for(int right=0;right<s.length();right++){
             char rightChar = s.charAt(right);
-            sMap.put(rightChar,sMap.getOrDefault(rightChar,0)+1);
-
-            if(pMap.containsKey(rightChar) && sMap.get(rightChar)<=pMap.get(rightChar)){
+            int val = map.getOrDefault(rightChar,0);
+            if(val>0){ 
                 count--;
             }
+            map.put(rightChar,val-1);
             int length = right - left + 1;
             if(length > p.length()){
-                char leftChar = s.charAt(left);
-                if(pMap.containsKey(leftChar) && sMap.get(leftChar)<=pMap.get(leftChar)){
-                    count++;
-                }
-            sMap.put(leftChar,sMap.get(leftChar)-1);
+            char leftChar = s.charAt(left);
+            int lefVal = map.getOrDefault(leftChar,0);
+            if(lefVal >=0){
+                count++;
+            }    
+            map.put(leftChar,lefVal+1);
             left++;
         }
         if(count==0){
