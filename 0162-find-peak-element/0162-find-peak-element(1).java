cbbcc6168c72b430ca1ca392fd81303999dd425d
K Deepak <deepakkarthik20@gmail.com>
2026-07-05 20:01:37 +0530

    Implement findPeakElement method in Solution class

---
+++ 0162-find-peak-element/0162-find-peak-element(1).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        //right side checking
+        int left = 0;
+        int right = nums.length - 1;
+        while(left<right){
+            int mid = left + (right-left+1)/2;
+            if(nums[mid] > nums[mid - 1]){
+                left = mid;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
